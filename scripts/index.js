10215c08a997b407bf328342ebf0a0131a01dd13
Alex Cap <jamurmansk@yandex.ru>
2023-02-15 17:20:06 +0300

    feat: make place cards uploading from js array

--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,35 @@ const descriptionInput = document.getElementById("description");
 const profileName = document.querySelector(".profile__name");
 const description = document.querySelector(".profile__description");
 
+const placeCardTemplate = document.querySelector("#place-card").content;
+const cardsGrid = document.querySelector(".elements__grid");
+const initialCards = [
+  {
+    name: "Карачаево-Черкессия",
+    link: "./images/karachaevsk.jpg",
+  },
+  {
+    name: "Гора Эльбрус",
+    link: "./images/elbrus.jpg",
+  },
+  {
+    name: "Домбай",
+    link: "./images/dombai.jpg",
+  },
+  {
+    name: "Мурманск",
+    link: "./images/murmansk.jpg",
+  },
+  {
+    name: "Абхазия",
+    link: "./images/abhazia.jpg",
+  },
+  {
+    name: "Красная поляна",
+    link: "./images/red-clearing.jpg",
+  }
+];
+
 function showEditModal() {
   editModal.classList.add("popup__opened");
   nameInput.value = profileName.innerText;
@@ -27,3 +56,17 @@ function saveProfileData(event) {
 editButton.addEventListener("click", showEditModal);
 closeButton.addEventListener("click", closeEditModal);
 popupForm.addEventListener("submit", saveProfileData);
+
+const renderCards = () => {
+  initialCards.map((item) => {
+    const placeCard = placeCardTemplate
+      .querySelector(".elements__element")
+      .cloneNode(true);
+    placeCard.querySelector(".elements__image").src = item.link;
+    placeCard.querySelector(".elements__name").textContent = item.name;
+
+    cardsGrid.append(placeCard);
+  });
+};
+
+renderCards();

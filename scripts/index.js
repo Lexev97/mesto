297da3627bedf26f3536daa276ae95e85744aece
Alex Cap <jamurmansk@yandex.ru>
2023-02-17 20:24:04 +0300

    feat: make addNewCard possibility

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,9 @@
 const editButton = document.querySelector(".profile__edit");
+const addNewPlaceButton = document.querySelector(".profile__add");
 const editModal = document.querySelector(".popup");
 const closeButton = document.querySelector(".popup__close");
+const saveButtonModal = document.querySelector(".popup__save");
+const popupTitle = document.querySelector(".popup__title");
 const popupForm = document.querySelector(".popup__form");
 const nameInput = document.getElementById("name");
 const descriptionInput = document.getElementById("description");
@@ -33,40 +36,73 @@ const initialCards = [
   {
     name: "Красная поляна",
     link: "./images/red-clearing.jpg",
-  }
+  },
 ];
 
-function showEditModal() {
-  editModal.classList.add("popup__opened");
-  nameInput.value = profileName.innerText;
-  descriptionInput.value = description.innerText;
-}
+const showModal = (e) => {
+  if (e.target.classList.value === "profile__edit") {
+    editModal.classList.add("popup__opened");
+    popupTitle.textContent = "Редактировать профиль";
+    nameInput.value = profileName.innerText;
+    descriptionInput.value = description.innerText;
+    saveButtonModal.textContent = "Сохранить";
+  } else {
+    editModal.classList.add("popup__opened");
+    popupTitle.textContent = "Новое место";
+    nameInput.placeholder = "Название";
+    descriptionInput.placeholder = "Ссылка на картинку";
+    saveButtonModal.textContent = "Создать";
+  }
+};
 
-function closeEditModal() {
+const closeEditModal = () => {
   editModal.classList.remove("popup__opened");
+  nameInput.value = "";
+  descriptionInput.value = "";
 }
 
-function saveProfileData(event) {
-  event.preventDefault();
-  profileName.textContent = nameInput.value;
-  description.textContent = descriptionInput.value;
-  closeEditModal();
+const saveDataHandler = (e) => {
+  e.preventDefault();
+  if (e.submitter.textContent === "Сохранить") {
+    profileName.textContent = nameInput.value;
+    description.textContent = descriptionInput.value;
+    closeEditModal();
+  } else {
+    initialCards.unshift({
+      name: nameInput.value,
+      link: descriptionInput.value,
+    });
+    renderCards();
+    closeEditModal();
+  }
 }
-
-editButton.addEventListener("click", showEditModal);
-closeButton.addEventListener("click", closeEditModal);
-popupForm.addEventListener("submit", saveProfileData);
 
 const renderCards = () => {
-  initialCards.map((item) => {
+  if (cardsGrid.childElementCount === 0) {
+    initialCards.forEach((item) => {
+      const placeCard = placeCardTemplate
+        .querySelector(".elements__element")
+        .cloneNode(true);
+      placeCard.querySelector(".elements__image").src = item.link;
+      placeCard.querySelector(".elements__name").textContent = item.name;
+
+      cardsGrid.append(placeCard);
+    });
+  } else if (cardsGrid.childElementCount < initialCards.length) {
     const placeCard = placeCardTemplate
       .querySelector(".elements__element")
       .cloneNode(true);
-    placeCard.querySelector(".elements__image").src = item.link;
-    placeCard.querySelector(".elements__name").textContent = item.name;
+    placeCard.querySelector(".elements__image").src = initialCards[0].link;
+    placeCard.querySelector(".elements__name").textContent =
+      initialCards[0].name;
 
-    cardsGrid.append(placeCard);
-  });
-};
+    cardsGrid.prepend(placeCard);
+  }
+}
+
+editButton.addEventListener("click", showModal);
+addNewPlaceButton.addEventListener("click", showModal);
+closeButton.addEventListener("click", closeEditModal);
+popupForm.addEventListener("submit", saveDataHandler);
 
 renderCards();
